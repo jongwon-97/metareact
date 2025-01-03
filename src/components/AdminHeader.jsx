b6fb4e01ce8d23c02e68efbad49ce1fff7422184
jongwon <jwsoshw@naver.com>
2025-01-03 10:52:01 +0900

    little

--- src/components/AdminHeader.jsx
+++ src/components/AdminHeader.jsx
@@ -13,7 +13,7 @@ const AdminHeader = () => {
     return (
     <nav className={`${styles.navbar} navbar navbar-dark bg-dark`}>
       <div className={styles.leftSection}>
-      <a className={`${styles.brand} navbar-brand ps-3`} href="/view/admin/dashboard">
+      <a className={`${styles.brand}`} href="/view/admin/dashboard">
         메타러닝 관리자 대시보드
       </a>
     
